7ae16ad5d024d2586229a4b603169897ea3bef08
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-10-07 23:21:25 +0300

    base structure

--- handler/status.go
+++ handler/status.go
@@ -1,1 +1,12 @@
 package handler
+
+import "github.com/rs/zerolog"
+
+type HealthCheckHandler struct {
+	logger *zerolog.Logger
+}
+
+func New(logger *zerolog.Logger) *HealthCheckHandler {
+	l := logger.With().Str("service", "health-check").Logger()
+	return &HealthCheckHandler{logger: &l}
+}
