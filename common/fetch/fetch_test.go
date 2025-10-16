2cff54b2f2a009c02ca5ca776acf0647f189a184
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-10-16 23:49:57 +0300

    base success test for fetcher

--- common/fetch/fetch_test.go
+++ common/fetch/fetch_test.go
@@ -1,1 +1,34 @@
 package fetch
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFetcher(t *testing.T) {
+	t.Parallel()
+
+	// Initialize the logger instance using zerolog to output logs to the standard output.
+	// This logger will be used for logging purposes throughout the test run.
+	logger := zerolog.New(os.Stdout)
+
+	t.Run("Success", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK"))
+		}))
+		defer server.Close()
+
+		fetcher := New(&logger)
+
+		response, err := fetcher.Fetch(context.Background(), server.URL)
+		assert.NoError(t, err)
+		assert.NotNil(t, response)
+	})
+}
