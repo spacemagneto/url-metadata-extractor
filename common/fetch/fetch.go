b5ca46903752e0102e47393b445d82941c1fe102
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-10-16 17:07:21 +0300

    base fetcher pkg

---
+++ common/fetch/fetch.go
@@ -0,0 +1,46 @@
+package fetch
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+
+	"github.com/rs/zerolog"
+)
+
+type Fetcher struct {
+	client *http.Client
+	logger *zerolog.Logger
+}
+
+func New(log *zerolog.Logger) *Fetcher {
+	l := log.With().Str("service", "http").Logger()
+
+	return &Fetcher{logger: &l, client: &http.Client{}}
+}
+
+func (f *Fetcher) Fetch(ctx context.Context, link string) (*http.Response, error) {
+	l := f.logger.With().Str("method", "Fetch").Logger()
+
+	u, err := url.Parse(link)
+	if err != nil {
+		l.Error().Err(err).Msg("failed parse link")
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := f.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (f *Fetcher) doRequest(req *http.Request) (*http.Response, error) {
+	return f.client.Do(req)
+}
