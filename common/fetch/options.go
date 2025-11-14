76f79333d9e36f6da2fed29cdc074ffc950a6871
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-11-14 18:19:41 +0300

    base client options struct

--- common/fetch/options.go
+++ common/fetch/options.go
@@ -1,1 +1,18 @@
 package fetch
+
+import (
+	"net/http"
+	"time"
+)
+
+type ClientOptions struct {
+	DialTimeout           time.Duration
+	TLSHandshakeTimeout   time.Duration
+	ResponseHeaderTimeout time.Duration
+	ExpectContinueTimeout time.Duration
+	IdleConnTimeout       time.Duration
+	MaxIdleConnect        int
+	MaxIdleConnectPerHost int
+	MaxConnectPerHost     int
+	CheckRedirect         func(req *http.Request, via []*http.Request) error
+}
