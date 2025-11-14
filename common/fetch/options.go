3877dbe67695b7df292ddfd295b1cceaa1be6411
syntaxErrorLineNULL <u5495527112@gmail.com>
2025-11-14 18:20:42 +0300

    DefaultClientOptions

--- common/fetch/options.go
+++ common/fetch/options.go
@@ -16,3 +16,16 @@ type ClientOptions struct {
 	MaxConnectPerHost     int
 	CheckRedirect         func(req *http.Request, via []*http.Request) error
 }
+
+func DefaultClientOptions() ClientOptions {
+	return ClientOptions{
+		DialTimeout:           10 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 2 * time.Second,
+		IdleConnTimeout:       90 * time.Second,
+		MaxIdleConnect:        100,
+		MaxIdleConnectPerHost: 25,
+		MaxConnectPerHost:     50,
+	}
+}
